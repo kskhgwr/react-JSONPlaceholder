87c3b8b5912a5cd8474123b2a0e3bbafbf250b21
kskhgwr <77972033+kskhgwr@users.noreply.github.com>
2021-06-24 21:59:37 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import axios from "axios";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res.data);
+    });
+  };
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users/1").then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+  );
+}
